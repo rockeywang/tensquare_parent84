279fe2f3ba90a9164f7b5fcb6f045882c21833d4
wangchao <wcwangsm@163.com>
2019-02-03 16:33:46 +0800

    添加文章模块

---
+++ tensquare_search/src/main/java/com/tensquare/search/service/ArticleService.java
@@ -0,0 +1,33 @@
+package com.tensquare.search.service;
+
+import com.tensquare.search.dao.ArticleDao;
+import com.tensquare.search.pojo.Article;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by wangchao on 2019/2/3.
+ */
+@Service
+public class ArticleService {
+
+
+    @Autowired
+    private ArticleDao articleDao;
+
+    public void save(Article article){
+        //article.setId(idWorker.nextId()+"");
+        articleDao.save(article);
+    }
+
+    public Page<Article> findByKey(String key, int page, int size) {
+        Pageable pageable = PageRequest.of(page-1, size);
+        return articleDao.findByTitleOrContentLike(key, key, pageable);
+    }
+
+
+
+}
